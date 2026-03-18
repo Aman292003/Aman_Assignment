cd714a46bb784ff1ab57ea7ce3af7391d6935906
Aman292003 <anandaman792@gmail.com>
2026-03-18 14:19:10 +0530

    18-march

--- 16-March/UniversityCourseApp/obj/Debug/net8.0/UniversityCourseApp.AssemblyInfo.cs
+++ 16-March/UniversityCourseApp/obj/Debug/net8.0/UniversityCourseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversityCourseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1a607fa1a931b524892ee1571d60903b9cf70f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23820d0e52669f8f518755d2c7ff86704b5df251")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversityCourseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversityCourseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 16-March/mvcefdemo/Controllers/NORTHWNDController1.cs
+++ 16-March/mvcefdemo/Controllers/NORTHWNDController1.cs
@@ -41,9 +41,68 @@ namespace mvcefdemo.Controllers
             //     CompanyName = x.CompanyName
             // });
             var query1 = searchCustomer.Single();
-           // var query2 = searchcustomer2.Single();
+            // var query2 = searchcustomer2.Single();
             return View(query1);
 
+        }
+        public ActionResult ProductInCategory(string categoryname)
+        {
+            NORTHWNDContext cnt = new NORTHWNDContext();
+
+            if (string.IsNullOrEmpty(categoryname))
+                return View(new List<ProdCat>());
+
+            var prod = cnt.Products
+                .Where(x => x.Category.CategoryName == categoryname)
+                .Select(c => new ProdCat
+                {
+                    prodname = c.ProductName,
+                    catname = c.Category.CategoryName
+                }).ToList();
+
+            return View(prod);
+        }
+        public ActionResult OrderRange(string range) {
+
+            NORTHWNDContext cnt = new NORTHWNDContext();
+            var range1 = Convert.ToInt16(range);
+            var custorder = cnt.Customers
+                .Where(x => x.Orders.Count > range1)
+                .Select(c => new Customer
+                {
+                    CustomerId = c.CustomerId,
+                    ContactName = c.ContactName
+                });
+
+
+            return View(custorder);
+        }
+        public IActionResult CustomerOrderDetails(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                return View(new List<Order>()); // return empty if no customer
+
+            NORTHWNDContext cnt = new NORTHWNDContext();
+
+            // Fetch all orders for the given customer
+            var orders = cnt.Orders
+                .Where(o => o.CustomerId == customerId)
+                .Select(o => new Order
+                {
+                    OrderId = o.OrderId,
+                    OrderDate = o.OrderDate,
+                    RequiredDate = o.RequiredDate,
+                    ShippedDate = o.ShippedDate,
+                    ShipName = o.ShipName,
+                    ShipCity = o.ShipCity,
+                    Freight = o.Freight
+                })
+                .ToList();
+
+            ViewBag.CustomerId = customerId;
+            ViewBag.OrderCount = orders.Count;
+
+            return View(orders);
         }
     }
 
