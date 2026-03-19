296aab4df85420cf19fa0abdcb78556b00e9bfa2
Aman292003 <anandaman792@gmail.com>
2026-03-19 15:42:59 +0530

    19-march

---
+++ 19-March/CodeFirsrEF/Models/Course1.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirsrEF.Models
+{
+    public class Course1
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]   
+        
+        public int Id { set; get; }
+        
+        [Required]
+        [Column("Stitle" ,TypeName = "varchar")]
+        public string Title { set; get; }
+
+        public string Description { set; get; }
+
+        public float fullprice { set; get; }
+
+        public Author1 author { set; get; }
+       
+        [ForeignKey("Author")]
+
+        public int AuthorId { set; get; }
+    }
+}
---
+++ 19-March/CodeFirsrEF/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirsrEF.Models
+{
+    public class Customer
+    {
+        public int CustomerId { set; get; }
+
+        [Required]
+        public string CustomerName { set; get; }
+
+        public ICollection<Product> products { set; get; }
+    }
+}
