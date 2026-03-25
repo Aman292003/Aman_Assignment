b3a3b51225ee01e3a4c0e6966b2de22e5f9c47b6
Aman292003 <anandaman792@gmail.com>
2026-03-25 14:21:44 +0530

    25-march

---
+++ 25-march/WebAspIn/Models/EmpContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAspIn.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext(DbContextOptions dbContextOptions) :
+            base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> employees { set; get; }
+    }
+}
