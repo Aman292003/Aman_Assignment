1c151ba93fd83675ba01e8a2db04d1301e7852e6
Aman Anand <aman@local>
2026-04-02 17:39:54 +0530

    2April

---
+++ Week4/2-April/WebAspIn/Models/EmployeeBasicDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAspIn.Models
+{
+    public class EmployeeBasicDto
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public int Age { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ Week4/2-April/WebAspIn/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAspIn.Models
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
