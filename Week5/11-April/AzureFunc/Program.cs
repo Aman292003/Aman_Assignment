30b5db364e6ebed03e5ed385c0b62cfebfffcd09
Aman Anand <aman@local>
2026-04-11 11:42:37 +0530

    11-April

---
+++ Week5/11-April/AzureFunc/Program.cs
@@ -0,0 +1,18 @@
+using AzureFunc.Data;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+string connectionString = Environment.GetEnvironmentVariable("AzureSqlDatabase");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Build().Run();
